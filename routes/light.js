1cf8cf32a6697122409cf5fd2a9a0efe22339c23
kevintjinu <52851291+kevintjinu@users.noreply.github.com>
2021-08-27 11:21:35 +0530

    put api

--- routes/light.js
+++ routes/light.js
@@ -15,31 +15,46 @@ router.post("/light", async (req, res) => {
   }
 });
 
-router.get("/light", async ( res) => {
+router.get("/light", async ( req,res) => {
   try {
-    const response = await Light.find();
-    res.json({ status: "Success", message: response }).status(201);
+    const response = await Light.find({});
+    res.send(response).status(201);
   } catch (err) {
     console.log(err);
-    res.json({
-      status: "Failed",
-      error: err,
-    });
+    // res.json({
+    //   status: "Failed",
+    //   error: err,
+    // });
   }
 });
 
-router.patch('/light/:id', async(req,res)=>{
- const lightID= req.params.id
- const updates = Object.keys(req.body);
-    try{
-        const res = await Light.findById({_id: lightID}) 
-        updates.forEach((update) => (light[update] = req.body[update]));
-      await light.save();
-      res.send(light);
-    }catch(err){
-        res.send('Error')
-    }
+// router.patch('/light/:id', async(req,res)=>{
+//  const lightID= req.params.id
+//  const updates = Object.keys(req.body);
+//     try{
+//         const res = await Light.findById({_id: lightID}) 
+//         updates.forEach((update) => (light[update] = req.body[update]));
+//       await light.save();
+//       res.send(light);
+//     }catch(err){
+//         res.send('Error')
+//     }
 
+// });
+
+// router.put('/light/:id', function(req, res){
+//   try{
+//     res.send({type: 'PUT'});
+//   }catch(err){
+//     res.send('Error')
+// }
+router.put('/light/:id', function(req, res, next){
+  Light.findByIdAndUpdate({_id: req.params.id}, req.body).then(function(){
+      Light.findOne({_id: req.params.id}).then(function(light){
+          res.send(light);
+      });
+  }).catch(next);
 });
+// });
 
 module.exports = router;
